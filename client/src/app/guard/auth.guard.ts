473164820668f39cceb22931b9b4567c044a508f
Angelica-souza <angelicatecinfor@gmail.com>
2023-04-09 22:07:32 -0300

    Adicionando a verificação de autenticação usando guard

---
+++ client/src/app/guard/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = window.localStorage.getItem('token')
+    
+    if (token) return true
+
+    this.router.navigate(['login'])
+    return false
+  }
+}
+  
